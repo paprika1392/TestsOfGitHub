e436df10f9e83b5685ca9d28ab39594339d4e767
Sasha <aleksandrpaprika@gmail.com>
2024-04-23 15:38:52 +0400

    have been created login and password constants

--- untitled/src/test/java/Utils/Constants.java
+++ untitled/src/test/java/Utils/Constants.java
@@ -3,4 +3,6 @@ package Utils;
 public class Constants {
 
 public static final String GITHUB_HOME_PAGE_URL = "https://github.com/";
+public static final String LOGIN = "paprikan1991";
+public static final String PASSWORD = "basyrman1392";
 }
