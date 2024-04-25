c56cd9257360a6afe58133246fa0b65bf181d76b
Sasha <aleksandrpaprika@gmail.com>
2024-04-25 14:02:21 +0400

    have been changed locator signInButton

--- untitled/src/test/java/GitHubPages/StartPage.java
+++ untitled/src/test/java/GitHubPages/StartPage.java
@@ -1,12 +1,15 @@
 package GitHubPages;
 
+import Utils.Browser;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class StartPage extends BasePage {
 
-    private final By signInButton = By.xpath("//div[@class='flex-1']/a[@href='/login']");
+    private final By signInButton = By.xpath("//div[contains(@class,'mr-lg-3')]//a[@href='/login']");
 
     public void clickToSignInButton() {
+        Browser.getWebdriverWait().until(ExpectedConditions.elementToBeClickable(signInButton));
         driver.findElement(signInButton).click();
     }
 
